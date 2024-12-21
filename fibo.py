49f838faf434737507a56313e25144b7b53f16b1
Bhuvanakrishnan0102 <71762105009@cit.edu.in>
2024-12-21 21:00:13 +0530

    updated

--- fibo.py
+++ fibo.py
@@ -1,6 +1,8 @@
-def fibonacci_recursive(n):
+def fibonacci_iterative(n):
     if n <= 1:
         return n
-    return fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2)
-
-print(fibonacci_recursive(10))
+    a, b = 0, 1
+    for _ in range(2, n + 1):
+        a, b = b, a + b
+    return b
+print(fibonacci_iterative(10))
